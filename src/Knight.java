cfb56fee18ea8cd383c7d939ad6b3fc1c07f37ad
pinkbluesky <alina.li.usa@gmail.com>
2019-03-31 18:25:56 -0700

    Finish Knight move method

--- src/Knight.java
+++ src/Knight.java
@@ -9,6 +9,7 @@ public class Knight extends Piece
      * 2S & 1W, 1E<br>
      * 2E & 1N, 1S<br>
      * 2W & 1N, 1S<br>
+     * The knight can jump over other pieces.
      * 
      * @param color
      *            the color of the knight
@@ -21,6 +22,37 @@ public class Knight extends Piece
 
     public boolean canMove(Board board, Position p1, Position p2)
     {
+	// this part is for moving into an empty spot
+	if (board.isValid(p1) && board.isEmpty(p2) && !board.isEmpty(p1))
+	{
+	    Position twoMoves = board.moveNorth(p1, 2);
+	    // check 1W and 1E for North
+	    if (p2.equals(board.moveWest(twoMoves, 1)) || p2.equals(board.moveEast(twoMoves, 1)))
+	    {
+		return true;
+	    }
 
+	    twoMoves = board.moveSouth(p1, 2);
+	    // check 1W and 1E for South
+	    if (p2.equals(board.moveWest(twoMoves, 1)) || p2.equals(board.moveEast(twoMoves, 1)))
+	    {
+		return true;
+	    }
+
+	    twoMoves = board.moveEast(p1, 2);
+	    // check 1N and 1S for East
+	    if (p2.equals(board.moveNorth(twoMoves, 1)) || p2.equals(board.moveSouth(twoMoves, 1)))
+	    {
+		return true;
+	    }
+
+	    twoMoves = board.moveWest(p1, 2);
+	    // check 1N and 1S for West
+	    if (p2.equals(board.moveNorth(twoMoves, 1)) || p2.equals(board.moveSouth(twoMoves, 1)))
+	    {
+		return true;
+	    }
+	}
+	return false;
     }
 }
