f685ddfcade4184458fa676648a161b3a1f8cd06
pinkbluesky <46383286+pinkbluesky@users.noreply.github.com>
2019-04-17 20:52:43 -0700

    Create jframe

--- src/ChessViewer.java
+++ src/ChessViewer.java
@@ -1,27 +1,16 @@
+import javax.swing.JFrame;
+
 public class ChessViewer
 {
 
 	public static void main(String[] args)
 	{
 
-		ChessGame game = new ChessGame();
+		JFrame frame = new JFrame();
+		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		System.out.println(game);
-		// white
-		System.out.println(game.movePiece(new Position(6, 3), new Position(4, 3)));
-		System.out.println(game);
-		// black
-		System.out.println(game.movePiece(new Position(1, 1), new Position(2, 1)));
-		System.out.println(game);
-		// white
-		System.out.println(game.movePiece(new Position(4, 3), new Position(3, 3)));
-		System.out.println(game);
-		// black
-		System.out.println(game.movePiece(new Position(1, 2), new Position(3, 2)));
-		System.out.println(game);
-		// white
-		System.out.println(game.movePiece(new Position(3, 3), new Position(2, 2)));
-		System.out.println(game);
+		frame.setVisible(true);
 
 	}
 
