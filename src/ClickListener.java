a7a0af7d7d8cbed245fa238b7fecd648450f512b
pinkbluesky <46383286+pinkbluesky@users.noreply.github.com>
2019-04-19 22:08:24 -0700

    ClickListener done

--- src/ClickListener.java
+++ src/ClickListener.java
@@ -3,13 +3,45 @@ import java.awt.event.MouseListener;
 
 public class ClickListener implements MouseListener
 {
+	private boolean firstClick;
+	private ChessComp comp;
+
+	private int firstPixelX;
+	private int firstPixelY;
+
+	private String description;
+
+	public ClickListener(ChessComp comp)
+	{
+		firstClick = true;
+		this.comp = comp;
+		firstPixelX = 0;
+		firstPixelY = 0;
+		description = "";
+	}
+
+	public String getDescription()
+	{
+		return description;
+	}
 
 	@Override
 	public void mouseClicked(MouseEvent e)
 	{
 		// first click or second click?
+		if (firstClick)
+		{
+			description = "";
+			firstPixelX = e.getXOnScreen();
+			firstPixelY = e.getYOnScreen();
+		}
 		// move if second click
+		else
+		{
+			description = comp.movePiece(firstPixelX, firstPixelY, e.getXOnScreen(), e.getYOnScreen());
+		}
 
+		firstClick = !firstClick;
 	}
 
 	@Override
